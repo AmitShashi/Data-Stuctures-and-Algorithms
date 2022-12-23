6aa1a9084d7d7562cb285b570d08698b43bec1ea
AmitShashi <73923245+AmitShashi@users.noreply.github.com>
2022-12-23 18:39:13 +0530

    String - easy

---
+++ Largest Number.py
@@ -0,0 +1,23 @@
+'''
+Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+
+Since the result may be very large, so you need to return a string instead of an integer.
+
+Example 1:
+
+Input: nums = [10,2]
+Output: "210"
+Example 2:
+
+Input: nums = [3,30,34,5,9]
+Output: "9534330"
+ 
+'''
+class LargerNumKey(str):
+    def __lt__(x, y):
+        return x+y > y+x
+        
+class Solution:
+    def largestNumber(self, nums):
+        largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
+        return '0' if largest_num[0] == '0' else largest_num
