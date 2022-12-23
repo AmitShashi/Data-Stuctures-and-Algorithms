9168fcfd6be5cac54e168335c866fac7bdddc9bc
AmitShashi <73923245+AmitShashi@users.noreply.github.com>
2022-12-23 14:47:19 +0530

    array - easy

---
+++ Pair with equal sum.java
@@ -0,0 +1,47 @@
+/*
+1. You are given an array(arr) of distinct integers.
+2. You have to find if there are two pairs(A, B) and (C, D) present in the given array which satisfies the condition A+B = C+D.
+
+Sample Input
+8
+1 2 998 72 87576 21 45 -1
+
+Sample Output
+false
+*/
+import java.util.*;
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) 
+        {
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+
+    public static boolean solution(int[] arr) 
+    {
+    HashSet<Integer> set = new HashSet<>();
+		for(int i = 0 ; i < arr.length ; i++) 
+    {
+			for(int j = i + 1; j < arr.length ; j++) 
+      {
+				int sum = arr[i] + arr[j];
+				if(set.contains(sum))
+        {
+				    return true;
+				}
+        else
+        {
+				    set.add(sum);
+				}
+			}
+		}
+		return false;
+    }
+}
