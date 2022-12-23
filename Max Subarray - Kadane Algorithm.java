d352ac696f133d910495f458f423d26ada064c54
AmitShashi <73923245+AmitShashi@users.noreply.github.com>
2022-12-23 14:32:25 +0530

    array - easy - medium

---
+++ Max Subarray - Kadane Algorithm.java
@@ -0,0 +1,28 @@
+/*
+Given an integer array nums, find the subarray
+which has the largest sum and return its sum.
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+*/
+  
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // Initialize our variables using the first element.
+        int currentSubarray = nums[0];
+        int maxSubarray = nums[0];
+        
+        // Start with the 2nd element since we already used the first one.
+        for (int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            // If current_subarray is negative, throw it away. Otherwise, keep adding to it.
+            currentSubarray = Math.max(num, currentSubarray + num);
+            maxSubarray = Math.max(maxSubarray, currentSubarray);
+        }
+        
+        return maxSubarray;
+    }
+}
