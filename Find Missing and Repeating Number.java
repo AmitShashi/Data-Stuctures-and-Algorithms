cf4b5050520d1680a8dafcdcd3b1a964d7d3f10c
AmitShashi <73923245+AmitShashi@users.noreply.github.com>
2022-12-23 13:30:51 +0530

    array - easy

---
+++ Find Missing and Repeating Number.java
@@ -0,0 +1,24 @@
+
+// Java program to Find the repeating and missing elements
+ 
+static void printTwoElements(int arr[], int size)
+{
+  int i;
+  System.out.print("The repeating element is ");
+ 
+  for (i = 0; i < size; i++) 
+  {
+    int abs_val = Math.abs(arr[i]);
+    if (arr[abs_val - 1] > 0)
+      arr[abs_val - 1] = -arr[abs_val - 1];
+    else
+      System.out.println(abs_val);
+  }
+  System.out.print("and the missing element is ");
+  for (i = 0; i < size; i++) 
+  {
+    if (arr[i] > 0)
+      System.out.println(i + 1);
+  }
+}
+//Note: we can also use map
