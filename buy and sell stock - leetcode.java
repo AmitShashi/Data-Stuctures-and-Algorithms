c2016ec480a4ae29fb0239cb279138224dc6a5c1
AmitShashi <73923245+AmitShashi@users.noreply.github.com>
2022-12-26 02:48:59 +0530

    DP - Medium, Hard

---
+++ buy and sell stock - leetcode.java
@@ -0,0 +1,18 @@
+/*Best Time to Buy and Sell Stock - LEETCODE*/
+
+class Solution 
+{
+    public int maxProfit(int[] A) 
+    {
+        int minPrice = A[0];
+        int maxProfit = 0;
+        for(int i=1; i<A.length; i++)
+        {
+            minPrice = Math.min(minPrice , A[i]);
+            int curProfit = A[i]- minPrice;
+            maxProfit = Math.max(curProfit,maxProfit);
+
+        }
+        return maxProfit;
+    }
+}
