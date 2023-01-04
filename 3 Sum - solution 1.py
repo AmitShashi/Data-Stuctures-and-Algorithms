41dc0a09db1edb609281ced99289b174c9118a98
AmitShashi <73923245+AmitShashi@users.noreply.github.com>
2023-01-04 18:11:40 +0530

    Update 3 Sum - solution 1.py

--- 3 Sum - solution 1.py
+++ 3 Sum - solution 1.py
@@ -3,6 +3,7 @@ Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
 such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0
 '''
 class Solution(object):
+    
     def threeSum(self, nums):
         target = 0 # given in question
         res = []
