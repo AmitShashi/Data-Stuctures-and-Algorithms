5238f34beca57a40fadb6ec9976a5cc99c73663b
AmitShashi <73923245+AmitShashi@users.noreply.github.com>
2023-01-26 19:24:20 +0530

    dp , memo

--- word break.py
+++ word break.py
@@ -3,8 +3,6 @@ Given a string s and a dictionary of strings wordDict, return true if s can be s
 
 Note that the same word in the dictionary may be reused multiple times in the segmentation.
 
- 
-
 Example 1:
 
 Input: s = "leetcode", wordDict = ["leet","code"]
